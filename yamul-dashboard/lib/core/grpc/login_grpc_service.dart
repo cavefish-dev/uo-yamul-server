119bca4701a9392aa8400829b0ef86cc6695cb2b
cavefish <cavefish@gmail.com>
2026-03-20 14:55:35 +0100

    feat: derive gRPC endpoint from page origin for Docker compatibility

--- yamul-dashboard/lib/core/grpc/login_grpc_service.dart
+++ yamul-dashboard/lib/core/grpc/login_grpc_service.dart
@@ -3,10 +3,13 @@ import 'package:uo_yamul_dashboard/generated/grpc/yamul-dashboard-login.pbgrpc.d
 
 class LoginGrpcService {
   static Future<DashboardLoginServiceClient> createClient() async {
-
-    final channel = GrpcWebClientChannel.xhr(
-      Uri.parse('http://localhost:8091'),
+    final grpcUri = Uri(
+      scheme: Uri.base.scheme,
+      host: Uri.base.host,
+      port: Uri.base.port,
+      path: '/grpc/',
     );
+    final channel = GrpcWebClientChannel.xhr(grpcUri);
     return DashboardLoginServiceClient(channel);
   }
 }
